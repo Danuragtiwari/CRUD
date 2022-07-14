019688dd1dd5484b7dfc96ec7d82e9ebd0411d3c
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-07-14 19:17:22 +0530

    4th commit

--- app1/models.py
+++ app1/models.py
@@ -5,6 +5,6 @@ class Student(models.Model):
     name=models.CharField(max_length=10)
     rollno=models.CharField(max_length=5)
     contact=models.CharField(max_length=10)
-    standard=models.IntegerField(max_length=2)
+    standard=models.CharField(max_length=2)
     class Meta:
         db_table="student"
--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,10 @@ from .models import Student
 
 # Create your views here.
 def add(request):
+    if request.method=='POST':
+        form=StudentForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+
     return render(request,'add.html')
