ca4dcdee7263f85a1d8a288029dc14e6ba11e20d
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-07-14 18:55:18 +0530

    third commit

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=10)
+    rollno=models.CharField(max_length=5)
+    contact=models.CharField(max_length=10)
+    standard=models.IntegerField(max_length=2)
+    class Meta:
+        db_table="student"
--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect 
+from .forms import StudentForm
+from .models import Student
 
 # Create your views here.
 def add(request):
