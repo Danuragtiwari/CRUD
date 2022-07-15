1f7f602dd667f416b50ee6023c2af072fd30bc5f
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-07-15 13:53:59 +0530

    7th commit

--- app1/urls.py
+++ app1/urls.py
@@ -3,9 +3,13 @@ from django.urls import path,include
 from .views import *
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('',app,name='app'),
-    path('index/',index,name='index'),#index=emp
-    path('home/',home,name='home')#home=show
+    # path('',app,name='app'),
+    path('',index,name='index'),#index=emp
+    path('home/',home,name='home'),#home=show
+    path('update/<int:id>',update,name='update'),
+    path('edit/<int:id>', edit,name='edit'),  
+    
+    path('delete/<int:id>', delete,name='delete'), 
     # path('/add',update,name='update')
     # path('/home',home)
 ]
--- app1/views.py
+++ app1/views.py
@@ -13,7 +13,7 @@ def index(request):
         if form.is_valid():
             try:
                form.save()
-               return redirect('/home')
+               return redirect('home/')
             except:
                 pass
 
@@ -23,3 +23,17 @@ def home(request):
     students=Student.objects.all()
     return render(request,'hometoshow.html',{'students':students})
     # return render(request,'add.html')
+def update(request,id):
+    students=StudentForm.objects.get(id=id)
+    form=StudentForm(request.POST,instance=students)
+    if form.is_valid():
+        form.save()
+        return redirect('home/')
+    return render(request,'hometoedit.html',{'student':students})
+def edit(request,id):
+    students=StudentForm.objects.get(id=id)
+    return render(request,'hometoedit.html',{'student':students})
+def delete(request,id):
+    students=Student.objects.get(id=id)
+    students.delete()
+    return redirect('home/')
